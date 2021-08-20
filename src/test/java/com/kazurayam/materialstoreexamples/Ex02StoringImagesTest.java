81609750c734f1c67cfd87f1df7f5c780017d1ea
kazurayam <kazuaki.urayama@gmail.com>
2021-08-20 10:59:16 +0900

    add umineko photo

--- src/test/java/com/kazurayam/materialstoreexamples/Ex02StoringImagesTest.java
+++ src/test/java/com/kazurayam/materialstoreexamples/Ex02StoringImagesTest.java
@@ -4,53 +4,74 @@ import com.kazurayam.materialstore.FileType;
 import com.kazurayam.materialstore.JobName;
 import com.kazurayam.materialstore.JobTimestamp;
 import com.kazurayam.materialstore.Metadata;
+import com.kazurayam.materialstore.MetadataImpl;
 import com.kazurayam.materialstore.Store;
 import com.kazurayam.materialstore.Stores;
 import com.kazurayam.subprocessj.Subprocess;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-public class Example01HelloTest {
+public class Ex02StoringImages {
 
     private static final String className =
-            Example01HelloTest.class.getSimpleName();
+            Ex01StoringStrings.class.getSimpleName();
 
     private static final Path root =
             Paths.get("./build/tmp/testOutput/" + className + "/store");
 
-    private Store store;
-    private JobName jobName;
-    private JobTimestamp jobTimestamp;
+    private static Store store;
+    private static JobName jobName;
+    private static JobTimestamp jobTimestamp;
 
     @BeforeAll
     public static void beforeAll() throws IOException {
         TestUtils.initDir(root);
-    }
-
-    @BeforeEach
-    public void beforeEach() {
         store = Stores.newInstance(root);
         jobName = new JobName(className);
         jobTimestamp = JobTimestamp.now();
     }
 
+    @BeforeEach
+    public void beforeEach() {}
+
     @Test
-    public void test_write_a_text_file() {
-        String content = "Hello, world!";
-        store.write(jobName, jobTimestamp, FileType.TXT, Metadata.NULL_OBJECT,
-                content, StandardCharsets.UTF_8);
+    public void test_storing_a_photo() throws Exception {
+        URL url = new URL("https://photo53.com/img/fushimiinari27.jpg");
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int n = 0;
+            byte [] buffer = new byte[ 1024 ];
+            while (-1 != (n = inputStream.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+        }
+        byte[] bytes = output.toByteArray();
+
+        Metadata metadata = new MetadataImpl.Builder(url)
+                .put("location", "京都 伏見稲荷")
+                .put("title", "千本鳥居")
+                .build();
+
+        store.write(jobName, jobTimestamp, FileType.TXT, metadata, bytes);
     }
 
     @AfterEach
-    public void after_each() throws IOException, InterruptedException {
+    public void afterEach() {}
+
+    @AfterAll
+    public static void afterAll() throws IOException, InterruptedException {
         Subprocess subprocess = new Subprocess();
         subprocess.cwd(new File(root.toString()));
         Subprocess.CompletedProcess cp = subprocess.run(Arrays.asList("tree", "."));
@@ -60,7 +81,4 @@ public class Example01HelloTest {
         TestUtils.writeLines(cp.stdout(), tree);
         TestUtils.copyIndex(store, jobName, jobTimestamp, index);
     }
-
-
-
 }
