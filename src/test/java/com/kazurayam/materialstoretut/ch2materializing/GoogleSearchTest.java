c70286d15307732ad1951851c09c72608d95a07d
kazurayam <kazuaki.urayama@gmail.com>
2022-04-15 14:05:27 +0900

    extended GoogleSearch

--- src/test/java/com/kazurayam/materialstoretut/ch2materializing/GoogleSearchTest.java
+++ src/test/java/com/kazurayam/materialstoretut/ch2materializing/GoogleSearchTest.java
@@ -1,7 +1,9 @@
 package com.kazurayam.materialstoretut.ch2materializing;
 
+import com.kazurayam.materialstore.filesystem.FileType;
 import com.kazurayam.materialstore.filesystem.JobName;
 import com.kazurayam.materialstore.filesystem.JobTimestamp;
+import com.kazurayam.materialstore.filesystem.Metadata;
 import com.kazurayam.materialstore.filesystem.Store;
 import com.kazurayam.materialstore.filesystem.Stores;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,16 +12,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A Selenium Test that does the following:
@@ -61,12 +68,33 @@ public class GoogleSearchTest {
     @Test
     public void test_google_for_Shohei_Ohtani() throws Exception {
         WebDriverWait wait = new WebDriverWait(driver, 10);
+        // open the Google Search page
         URL entryURL = new URL("https://www.google.com");
         driver.navigate().to(entryURL);
+        // set a query into the <input name="q">
         By by_input_q = By.cssSelector("input[name=\"q\"]");
         wait.until(ExpectedConditions.visibilityOfElementLocated(by_input_q));
         WebElement we_input_q = driver.findElement(by_input_q);
-        if (we_input_q == null) throw new IllegalStateException();
+        we_input_q.sendKeys("Shohei Ohtani");
+        // take the screenshot of the Google Search page,
+        TakesScreenshot scrShot = (TakesScreenshot) driver;
+        File srcFile1 = scrShot.getScreenshotAs(OutputType.FILE);
+        // save the image into the store
+        Metadata metadata =
+                Metadata.builder(entryURL).put("step", "1").build();
+        store.write(jobName, jobTimestamp, FileType.PNG, metadata, srcFile1);
+        // send ENTER to execute a search request
+        we_input_q.sendKeys(Keys.chord(Keys.ENTER));
+        // wait for the search result page to load
+        By by_img_logo = By.xpath("//div[contains(@class,'logo')]/a/img");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by_img_logo));
+        // take screenshot of the result page, store the image into the store
+        File srcFile2 = scrShot.getScreenshotAs(OutputType.FILE);
+        // save the image into the store
+        URL resultPageURL = new URL(driver.getCurrentUrl());
+        Metadata metadata2 =
+                Metadata.builder(resultPageURL).put("step", "2").build();
+        store.write(jobName, jobTimestamp, FileType.PNG, metadata2, srcFile2);
     }
 
     @AfterEach
